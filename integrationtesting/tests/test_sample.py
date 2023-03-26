1e3a9b79e8d6bfc18c8ba318f095fc98b4b024a1
edsinkevic <sinkevicedwin@gmail.com>
2023-03-26 23:41:52 +0300

    Make integration test pass lmao

--- integrationtesting/tests/test_sample.py
+++ integrationtesting/tests/test_sample.py
@@ -24,7 +24,7 @@ def test_scenario():
     options.headless = True
     driver: WebDriver = webdriver.Chrome(options=options)
     driver.get(config.client_url)
-    driver.find_element(By.XPATH, "//p[contains(text(), 'Find in-depth')]")
+    driver.find_element(By.XPATH, "//*[contains(text(), 'invoiceEmail')]")
 
     response = requests.get(f"{config.server_url}/parcels/1")
     response.raise_for_status()
