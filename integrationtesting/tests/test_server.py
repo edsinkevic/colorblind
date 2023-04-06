122eb22d882fed7c2eaf8761b680e857ab7e2877
edsinkevic <sinkevicedwin@gmail.com>
2023-04-06 18:13:45 +0300

    Refactor IT a lil bit

--- integrationtesting/tests/test_server.py
+++ integrationtesting/tests/test_server.py
@@ -1,4 +1,5 @@
 import json
+from pprint import pprint
 
 import requests as requests
 from selenium import webdriver
@@ -26,25 +27,27 @@ def test_scenario():
                              headers={"Content-type": "application/json"})
     parcel_id = response.text
 
-    response = requests.post(f"{config.server_url}/parcels/{parcel_id}/submit/terminal/123",
-                             headers={"Content-type": "application/json", "If-Match": '"1"'})
+    response = requests.post(f"{config.server_url}/parcels/{parcel_id}/submit/terminal/123", headers=defaultHeaders(1))
+    pprint(response.text)
     response.raise_for_status()
 
-    response = requests.post(f"{config.server_url}/parcels/{parcel_id}/ship/123",
-                             headers={"Content-type": "application/json", "If-Match": '"2"'})
+    response = requests.post(f"{config.server_url}/parcels/{parcel_id}/ship/123", headers=defaultHeaders(2))
     response.raise_for_status()
 
-    response = requests.post(f"{config.server_url}/parcels/{parcel_id}/deliver",
-                             headers={"Content-type": "application/json", "If-Match": '"3"'})
+    response = requests.post(f"{config.server_url}/parcels/{parcel_id}/deliver", headers=defaultHeaders(3))
     response.raise_for_status()
+
+
+def defaultHeaders(version: int):
+    return {"Content-type": "application/json", "If-Match": f'''"{version}"'''}
 
 
 def defaultRegistration():
     return '''{"size": "S", "couponCode": "123", "transactionCode": "123",
-               "senderDeliveryInfo": {"email": "vardas@pavardaitis.com", "phoneNumber": "+37061095511",
-                                      "fullname": "Vardas Pavardaitis", "parcelLockerAddress": "Druskio g.5",
-                                      "takeawayAddress": ""},
-               "receiverDeliveryInfo": {"email": "vardas@pavardaitis.com", "phoneNumber": "+37061095511",
-                                        "fullname": "Vardas Pavardaitis", "parcelLockerAddress": "Druskio g.5",
-                                        "takeawayAddress": ""}, "invoiceEmail": "vardas@pavardaitis",
-               "deliveryType": {"from": "terminal", "to": "terminal"}}'''
+           "senderDeliveryInfo": {"email": "vardas@pavardaitis.com", "phoneNumber": "+37061095511",
+                                  "fullname": "Vardas Pavardaitis", "parcelLockerAddress": "Druskio g.5",
+                                  "takeawayAddress": ""},
+           "receiverDeliveryInfo": {"email": "vardas@pavardaitis.com", "phoneNumber": "+37061095511",
+                                    "fullname": "Vardas Pavardaitis", "parcelLockerAddress": "Druskio g.5",
+                                    "takeawayAddress": ""}, "invoiceEmail": "vardas@pavardaitis",
+           "deliveryType": {"from": "terminal", "to": "terminal"}}'''
