554e5eea67da83f0db3d2ecba1c51968b2622f66
edsinkevic <sinkevicedwin@gmail.com>
2023-04-08 09:49:04 +0300

    Add loading.tsx, add tracking page

--- client/src/app/track/[code]/page.tsx
+++ client/src/app/track/[code]/page.tsx
@@ -1,6 +1,4 @@
 import { detailsGetOne } from "colorblind/shared/requests/parcels";
-
-import * as E from "fp-ts/Either";
 import { ParcelDetails } from "colorblind/shared/lib/models/models";
 
 interface Props {
@@ -13,12 +11,5 @@ interface Props {
 export default async function ParcelDetailsPage({ params: { code } }: Props) {
   const parcelDetails = await detailsGetOne(code);
 
-  return (
-    <>
-      <h1>
-        {code}
-        {JSON.stringify(parcelDetails)}
-      </h1>
-    </>
-  );
+  return <>{JSON.stringify(parcelDetails)}</>;
 }
---
+++ client/src/app/track/page.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { FormInput } from "colorblind/shared/components/FormInput";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import styles from "./page.module.css";
+import { getFromStore, store } from "colorblind/shared/lib/state";
+import { PickerFromArray } from "colorblind/shared/components/PickerFromArray";
+
+interface Props {}
+
+export default function ParcelDetailsPage({}: Props) {
+  const [code, setCode] = useState<string>("");
+  const router = useRouter();
+  const [recentlyTracked, setRecentlyTracked] = useState<string[]>([]);
+
+  const storeRecentlyTracked = (code: string) => {
+    const recentlyTracked = getFromStore<string[]>("recentlyTracked");
+    if (!recentlyTracked) {
+      store("recentlyTracked", [code]);
+      return;
+    }
+
+    store("recentlyTracked", [...recentlyTracked, code]);
+  };
+
+  useEffect(() => {
+    const recentlyTracked = getFromStore<string[]>("recentlyTracked");
+
+    if (!recentlyTracked) {
+      setRecentlyTracked([]);
+      return;
+    }
+
+    setRecentlyTracked(recentlyTracked);
+    setCode(recentlyTracked.at(0) ?? "");
+  }, []);
+
+  return (
+    <div className={styles.form}>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          storeRecentlyTracked(code);
+          router.push(`/track/${code}`);
+        }}
+      >
+        <h1>Track</h1>
+        <FormInput
+          placeholder={"Code"}
+          value={code}
+          onChange={(e) => setCode(e.target.value)}
+        />
+        <PickerFromArray
+          hidden={recentlyTracked.at(0) === undefined}
+          array={recentlyTracked}
+          onSubmit={(code) => setCode(code)}
+        ></PickerFromArray>
+        <button type={"submit"} disabled={code == ""}>
+          Track
+        </button>
+      </form>
+    </div>
+  );
+}
