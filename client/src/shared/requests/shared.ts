9b44dd1deb358c8e3836c954e2b6f8008ab17fb7
edsinkevic <sinkevicedwin@gmail.com>
2023-04-29 10:41:04 +0300

    Unused

--- client/src/shared/requests/shared.ts
+++ client/src/shared/requests/shared.ts
@@ -1,5 +1,3 @@
-import { Problem } from "colorblind/shared/lib/models/models";
-
 export const colorblindServerUrl = (path: string) =>
   `${
     process.env.COLORBLIND_SERVER_URL ??
@@ -10,19 +8,4 @@ export const defaultFetchConfig = {
   headers: {
     "Content-Type": "application/json",
   },
-};
-
-export const defaultResponseHandler = async <T>(resp: Response): Promise<T> => {
-  if (resp.status === 400) {
-    const data = await resp.json();
-    throw new Error((data as Problem).title);
-  }
-
-  if (!resp.status.toString().startsWith("2")) {
-    const data = await resp.json();
-    throw new Error((data as Problem).title);
-  }
-
-  const data = await resp.json();
-  return data as T;
 };
