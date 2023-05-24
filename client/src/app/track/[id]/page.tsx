7f457395a2b12a00ff9b26a90f4afad9186749ee
karoluzas <79944650+karoluzas@users.noreply.github.com>
2023-05-24 16:54:49 +0300

    simple progress bar

--- client/src/app/track/[id]/page.tsx
+++ client/src/app/track/[id]/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { detailsGetOne } from "colorblind/shared/requests/parcels";
 import {
   ParcelDetails,
@@ -5,6 +7,8 @@ import {
 } from "colorblind/shared/lib/models/models";
 import { notFound } from "next/navigation";
 import styles from "./page.module.css";
+import { Progress } from "antd";
+
 
 interface Props {
   params: {
@@ -12,22 +16,36 @@ interface Props {
   };
 }
 
+
 export default async function ParcelDetailsPage({ params: { id } }: Props) {
   const parcelDetails = await detailsGetOne(id);
   const { status } = parcelDetails;
 
   if (status === StatusCodes.OK) {
+    let progressBar;
     const body = (await parcelDetails.json()) as ParcelDetails;
+    
     return (
       <div className={styles.info}>
         <div>
+          
           <h1>Order placed!</h1>
           <p>Your order code is: {body.code}</p>
           <p>
-            Place your order into the locker located at :
+            Order status :{body.status}
             {/* Retreive locker address based on terminalId */}
             {/* {body.senderDeliveryInfo.parcelLockerAddress} */}
           </p>
+          <Progress 
+            className={styles.progressBar} 
+            percent={body.version * 20} 
+            status="active"
+            strokeColor={{
+              "0%": "#9c2298",
+              "100%": "#5384b2"}}
+            size={[650, 20]}
+            showInfo={false}
+            />
         </div>
       </div>
     )
