2b3e8daa4799782d7d7cc6b8d92682171d49d618
edsinkevic <sinkevicedwin@gmail.com>
2023-05-30 10:27:37 +0300

    Prettify terminalparcels

--- client/src/app/terminal/[id]/courier/[courierId]/terminalparcels/page.tsx
+++ client/src/app/terminal/[id]/courier/[courierId]/terminalparcels/page.tsx
@@ -1,112 +1,186 @@
 "use client";
 
-import { ParcelDetails, ParcelDetailsForTerminal, Problem, StatusCodes } from "colorblind/shared/lib/models/models";
-import { detailsGetByTerminalId, getOneByCode, ship } from "colorblind/shared/requests/parcels";
+import {
+  ParcelDetails,
+  ParcelDetailsForTerminal,
+  Problem,
+  StatusCodes,
+} from "colorblind/shared/lib/models/models";
+import {
+  detailsGetByTerminalId,
+  detailsGetOne,
+  ship,
+} from "colorblind/shared/requests/parcels";
 import { notFound } from "next/navigation";
 import { MouseEventHandler, useEffect, useState } from "react";
-import styles from "./page.module.css";
+import styles from "colorblind/shared/styles/littleForms.module.scss";
+import { Button, Card, Col, List, Modal, Row } from "antd";
 
 interface Props {
-    params: {
-        id: string;
-        courierId: string;
-    };
+  params: {
+    id: string;
+    courierId: string;
+  };
 }
 
 export default function TerminalParcels({ params: { id, courierId } }: Props) {
-    const [parcels, setParcels] = useState<ParcelDetailsForTerminal[]>([]);
-    const [error, setError] = useState<string>();
+  const [parcels, setParcels] = useState<ParcelDetailsForTerminal[]>([]);
+  const [error, setError] = useState<string>();
 
-    const [selectedParcel, setSelectedParcel] = useState<ParcelDetails>();
-    const [lockerNumber, setLockerNumber] = useState<number>();
+  const [selectedParcel, setSelectedParcel] = useState<ParcelDetails>();
+  const [lockerNumber, setLockerNumber] = useState<number>();
 
-    useEffect(() => {
-        const initParcels = async () => {
-            const response = await detailsGetByTerminalId(id);
-            if (response.status !== StatusCodes.OK) {
-                notFound();
-            }
+  useEffect(() => {
+    const initParcels = async () => {
+      const response = await detailsGetByTerminalId(id);
+      if (response.status !== StatusCodes.OK) {
+        notFound();
+      }
 
-            const parcels = await response.json();
-            setParcels(parcels);
-        }
-
-        initParcels();
-    }, [id]);
-
-    const onShip: MouseEventHandler = async (e) => {
-        e.preventDefault();
-        if (error || !selectedParcel) return;
-        const response = await ship(selectedParcel.code, courierId, selectedParcel.version);
-        if (response.status !== StatusCodes.OK) {
-            const problem = (await response.json()) as Problem;
-            setError(problem.detail);
-            return;
-        }
-
-        const { lockerNumber } = await response.json() as { lockerNumber: number };
-
-        setLockerNumber(lockerNumber);
+      const parcels = await response.json();
+      setParcels(parcels);
     };
 
-    const onSelect: MouseEventHandler = async (e) => {
-        e.preventDefault();
-        const parcelResponse = await getOneByCode(e.currentTarget.id);
-        if (parcelResponse.status !== StatusCodes.OK) {
-            const problem = (await parcelResponse.json()) as Problem;
-            setError(problem.detail);
-            return;
-        }
+    initParcels();
+  }, [id]);
 
-        const parcel = (await parcelResponse.json()) as ParcelDetails;
+  const onShip: MouseEventHandler = async (e) => {
+    e.preventDefault();
+    if (error || !selectedParcel) return;
+    const response = await ship(
+      selectedParcel.code,
+      courierId,
+      selectedParcel.version
+    );
+    if (response.status !== StatusCodes.OK) {
+      const problem = (await response.json()) as Problem;
+      setError(problem.detail);
+      return;
+    }
 
-        setSelectedParcel(parcel);
+    const { lockerNumber } = (await response.json()) as {
+      lockerNumber: number;
     };
 
-    const onDone: MouseEventHandler = async (e) => {
-        e.preventDefault();
-        if (!lockerNumber) return;
+    setLockerNumber(lockerNumber);
+  };
 
-        const response = await detailsGetByTerminalId(id);
-        if (response.status !== StatusCodes.OK) {
-            notFound();
+  const onSelect: MouseEventHandler = async (e) => {
+    const parcelResponse = await detailsGetOne(e.currentTarget.id);
+    if (parcelResponse.status !== StatusCodes.OK) {
+      const problem = (await parcelResponse.json()) as Problem;
+      setError(problem.detail);
+      return;
+    }
+
+    const parcel = (await parcelResponse.json()) as ParcelDetails;
+
+    setSelectedParcel(parcel);
+  };
+
+  const onDone: MouseEventHandler = async (e) => {
+    e.preventDefault();
+    if (!lockerNumber) return;
+
+    const response = await detailsGetByTerminalId(id);
+    if (response.status !== StatusCodes.OK) {
+      notFound();
+    }
+
+    const parcels = await response.json();
+    setParcels(parcels);
+    setLockerNumber(undefined);
+    setSelectedParcel(undefined);
+  };
+
+  if (parcels.length === 0) {
+    return (
+      <div className={styles.info}>
+        <h1>No parcels in terminal</h1>
+      </div>
+    );
+  }
+
+  const SelectedComponent = () => {
+    return (
+      <Modal
+        centered
+        open={!!selectedParcel}
+        onCancel={() => setSelectedParcel(undefined)}
+        footer={
+          <Row justify={"space-evenly"}>
+            <Button key={"ship"} className={styles.bigButton} onClick={onShip}>
+              Ship
+            </Button>
+            <Button
+              disabled={!lockerNumber}
+              key={"confirm"}
+              className={styles.bigButton}
+              onClick={onDone}
+            >
+              Confirm shipment
+            </Button>
+          </Row>
         }
+      >
+        <Row justify={"center"}>
+          <span className={styles.selection}>Order selected</span>
+        </Row>
+        {lockerNumber && (
+          <Row justify={"center"}>
+            <span>Locker {lockerNumber} opened.</span>
+          </Row>
+        )}
+        <Row justify={"center"}>
+          <span>{selectedParcel?.id}</span>
+        </Row>
+      </Modal>
+    );
+  };
 
-        const parcels = await response.json();
-        setParcels(parcels);
-        setLockerNumber(undefined);
-        setSelectedParcel(undefined);
-    };
-
-    return (<div className={styles.info}>{
-        parcels.length === 0 ?
-            <h1>No parcels in terminal</h1>
-            : <div>
-                <h1>Parcels in terminal</h1>
-                {selectedParcel
-                    ? <div>
-                        <span>Order {selectedParcel.code} selected</span>
-                        {lockerNumber
-                            ? <div>
-                                <span>Locker {lockerNumber} opened.</span>
-                                <br />
-                                <button  className={styles.bigButton} onClick={onDone}>Parcel retrieved</button>
-                            </div>
-                            : <><br/><button  className={styles.bigButton} onClick={onShip}>Ship</button></>
-                        }
-                    </div>
-                    : <ul>
-                        {parcels.map((parcel) => <li key={parcel.id}>
-                            <span>Parcel id: </span>
-                            {parcel.id}
-                            <br />
-                            <span>Delivery address: </span>
-                            {parcel.deliveryTerminalAddress}
-                            <br />
-                            <button className={styles.bigButton} onClick={onSelect} id={parcel.code}>Select</button>
-                        </li>)}
-                    </ul>
-                }
-            </div>
-    }</div>);
+  return (
+    <>
+      <SelectedComponent />
+      <Row>
+        <Col className={styles.terminalCatalogue}>
+          <Row justify={"center"}>
+            <span className={styles.title}>Parcels in terminal</span>
+          </Row>
+          <List
+            grid={{
+              gutter: 16,
+              column: 2,
+            }}
+            pagination={{
+              pageSize: 4,
+              onChange: (page) => {
+                console.log(page);
+              },
+            }}
+            dataSource={parcels}
+            renderItem={(parcel) => (
+              <List.Item>
+                <Card
+                  title={
+                    <Row justify={"center"}>
+                      <span>Parcel</span>
+                    </Row>
+                  }
+                  onClick={onSelect}
+                  className={styles.clickableCard}
+                  bodyStyle={{ padding: "10px" }}
+                  id={parcel.id}
+                  hoverable
+                >
+                  <Row justify={"center"}>
+                    <span>To: {parcel.deliveryTerminalAddress}</span>
+                  </Row>
+                </Card>
+              </List.Item>
+            )}
+          />
+        </Col>
+      </Row>
+    </>
+  );
 }
--- client/src/app/terminal/page.tsx
+++ client/src/app/terminal/page.tsx
@@ -9,30 +9,33 @@ import {
 import { getAll } from "colorblind/shared/requests/terminal";
 import { Button, Form, Row, Select } from "antd";
 import styles from "colorblind/shared/styles/littleForms.module.scss";
+import useNotification from "antd/es/notification/useNotification";
+import { defaultError } from "colorblind/shared/notifications/defaults";
 
 export default function StepOne() {
   const router = useRouter();
-  const [problem, setProblem] = useState<Problem>();
-  const [error, setError] = useState<string>();
   const [terminals, setTerminals] = useState<TerminalDetails[]>([]);
   const [pickedTerminalId, setPickedTerminalId] = useState<string>();
+  const [notificationApi, notificationContext] = useNotification();
+
   useEffect(() => {
     const fetchTerminals = async () => {
       const response = await getAll();
 
       if (response.status !== StatusCodes.OK) {
         const body = (await response.json()) as Problem;
-        setProblem(body);
+        defaultError(notificationApi, body);
         return;
       }
 
       const terminals = (await response.json()) as TerminalDetails[];
       setTerminals(terminals);
-      setPickedTerminalId(terminals[0].id);
+
+      setPickedTerminalId(terminals[0]?.id);
     };
 
     fetchTerminals();
-  }, []);
+  }, [notificationApi]);
 
   const terminalOptions = terminals.map((terminal) => ({
     value: terminal.id,
@@ -47,6 +50,7 @@ export default function StepOne() {
         router.push(`/terminal/${pickedTerminalId}`);
       }}
     >
+      {notificationContext}
       <Row justify={"center"}>
         <span className={styles.title}>Select a terminal</span>
       </Row>
@@ -62,14 +66,12 @@ export default function StepOne() {
       <Row justify={"center"}>
         <Button
           htmlType="submit"
-          disabled={!pickedTerminalId || !!error || !!problem}
+          disabled={!pickedTerminalId}
           className={styles.bigButton}
         >
           Submit
         </Button>
       </Row>
-      {problem ? JSON.stringify(problem) : null}
-      {error}
     </Form>
   );
 }
